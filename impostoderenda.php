4aa8b4237fc8c87dc4174ce65c68e68c75c2a23e
leonardoburgos1 <Leonardoburgossantos@gmail.com>
2026-03-17 22:17:17 -0300

    primeiro comiit que eu fiz com ajuda do dani :rocket:

--- impostoderenda.php
+++ impostoderenda.php
@@ -1,48 +1,24 @@
 <?php 
 	
-	$sal = 10000;
-	
+	$sal = 3500;
 
+	function impostoRenda($pct, $sal) {
+		$imposto = $sal * $pct / 100;
+		$sali = $sal - $imposto;
+		printf("Salário bruto: R$%.2f <br/>
+				Porcetagem de desconto: %.1f%%<br/>
+				Valor do desconto: R$%.2f <br/>
+				Salário líquido: R$%.2f", $sal, $pct, $imposto, $sali);
+	}
 	if ($sal >= 3036 && $sal < 3533.31) {
-		$pct = 7.5;
-		$imposto = $sal * $pct / 100;
-		$sali = $sal - $imposto;
-		printf("Salário bruto: R$%.2f <br/>
-				Porcetagem de desconto: %.1f%%<br/>
-				Valor do desconto: R$%.2f <br/>
-				Salário líquido: R$%.2f", $sal, $pct, $imposto, $sali);
+		impostoRenda(7.5, $sal);
 	} elseif ($sal >= 3533.31 && $sal < 4688.85) {
-		$pct = 15;
-		$imposto = $sal * $pct / 100;
-		$sali = $sal - $imposto;
-		printf("Salário bruto: R$%.2f <br/>
-				Porcetagem de desconto: %.1f%%<br/>
-				Valor do desconto: R$%.2f <br/>
-				Salário líquido: R$%.2f", $sal, $pct, $imposto, $sali);
+		impostoRenda(15, $sal);
 	} elseif ($sal >= 4688.85 && $sal < 5830.85) {
-		$pct = 22.5;
-		$imposto = $sal * $pct / 100;
-		$sali = $sal - $imposto;
-		printf("Salário bruto: R$%.2f <br/>
-				Porcetagem de desconto: %.1f%%<br/>
-				Valor do desconto: R$%.2f <br/>
-				Salário líquido: R$%.2f", $sal, $pct, $imposto, $sali);
+		impostoRenda(22.5, $sal);
 	} elseif ($sal >= 5830.85) {
-		$pct = 27.5;
-		$imposto = $sal * $pct / 100;
-		$sali = $sal - $imposto;
-		printf("Salário bruto: R$%.2f <br/>
-				Porcetagem de desconto: %.1f%%<br/>
-				Valor do desconto: R$%.2f <br/>
-				Salário líquido: R$%.2f", $sal, $pct, $imposto, $sali);
+		impostoRenda(27.5, $sal);
 	} else {
-		$pct = 0;
-		$imposto = 0;
-		$sali = $sal - $imposto;
-		printf("Salário bruto: R$%.2f <br/>
-				Porcetagem de desconto: %.1f%%<br/>
-				Valor do desconto: R$%.2f <br/>
-				Salário líquido: R$%.2f", $sal, $pct, $imposto, $sali);
-		//comentario
+		impostoRenda(0, $sal);
 	}
  ?>
